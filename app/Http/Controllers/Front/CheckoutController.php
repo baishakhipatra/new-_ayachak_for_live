68cc773415d8e6c22e3370b49ae7f548475f98ae
Puja <puja@techmantra.co>
2025-09-16 19:42:03 +0530

    Guest Checkout

--- app/Http/Controllers/Front/CheckoutController.php
+++ app/Http/Controllers/Front/CheckoutController.php
@@ -242,14 +242,25 @@ class CheckoutController extends Controller
             // $checkoutData = $request->except('_token');
             $order_id = $this->checkoutRepository->create($checkoutData);
             $order = Order::with(['orderProducts.productDetails.category'])->findOrFail($order_id);
-            //dd($order);
             // try {
             //     Mail::to($order->email)->send(new OrderConfirmationMail($order));
             // } catch (\Exception $e) {
             //     \Log::error("Mail sending failed: " . $e->getMessage());
             // }
 
-            return view('front.checkout.complete', compact('order_id','order'))->with('success', 'Thank you for you order');
+
+            // $order = [];
+            // $email_data = [
+            //     'name' => 'Sagar Shah',
+            //     'subject' => 'Onn - Franchise partner request',
+            //     'email' => 'puja17kumari2025@gmail.com',
+            //     'blade_file' => 'front.emails.order_confirmation',
+            //     'order'      => $order, 
+            // ];
+            SendMail($order);
+            // $order_id = 1;
+          
+            // return view('front.checkout.complete', compact('order_id','order'))->with('success', 'Thank you for you order');
 
         }else{
             return redirect()->back()->with('failure', 'Something happened.Try again.');
